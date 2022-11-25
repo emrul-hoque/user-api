6ac52836feb303565d18cd1e1c64dffc0ab2d141
emrul <emrul.hoque@gmail.com>
2022-11-25 12:05:58 -0600

    initial implementation

---
+++ My.User.Api/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My.User.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        private List<Models.User> _users = new() {
+            new Models.User() { Id= 1, Name = "Jon", Email = "jon.doe@email.com" },
+            new Models.User() { Id= 2, Name = "Jane", Email = "jane.doe@email.com" }
+        };
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get all users
+        /// </summary>
+        /// <returns>A collection of user</returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var result = await Task.FromResult(_users);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get a user by id
+        /// </summary>
+        /// <param name="id">The user id</param>
+        /// <returns>A user</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var result = await Task.FromResult(_users.FirstOrDefault(u => u.Id == id));
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# user-api
+# My User API
+
+---
+- [x] Create a API project
+- [x] Add swagger page
+- [ ] Add CRUD endpoints
+---
+* [Tutorial: Create a web API with ASP.NET Core](https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?WT.mc_id=dotnet-35129-website&view=aspnetcore-7.0&tabs=visual-studio)
