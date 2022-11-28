8e5e1bd3fc9716eee7e8fdfc3cef91f89ffe3115
emrul <emrul.hoque@gmail.com>
2022-11-27 20:42:36 -0600

    added rest of the CRUD endpoints

--- My.User.Api/Controllers/UsersController.cs
+++ My.User.Api/Controllers/UsersController.cs
@@ -2,6 +2,15 @@
 
 namespace My.User.Api.Controllers
 {
+
+    /*
+     * http://localhost:{PORT}/api/users/1
+     * 
+     * Base URL: http://localhost:{PORT}
+     * Path: /api/users
+     * Path/Route Param: 1
+     */
+
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
@@ -20,6 +29,7 @@ namespace My.User.Api.Controllers
 
         /// <summary>
         /// Get all users
+        /// Path: /api/users/
         /// </summary>
         /// <returns>A collection of user</returns>
         [HttpGet]
@@ -31,6 +41,7 @@ namespace My.User.Api.Controllers
 
         /// <summary>
         /// Get a user by id
+        /// Path: /api/users/1
         /// </summary>
         /// <param name="id">The user id</param>
         /// <returns>A user</returns>
@@ -45,6 +56,48 @@ namespace My.User.Api.Controllers
             }
 
             return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.User user)
+        {
+            _users.Add(user);
+            return CreatedAtAction(nameof(Create), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Models.User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingUser = _users.FirstOrDefault(u => u.Id == id);
+
+            if (existingUser is null)
+            {
+                return NotFound();
+            }
+
+            // Update user here
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var existingUser = _users.FirstOrDefault(u => u.Id == id);
+
+            if (existingUser is null)
+            {
+                return NotFound();
+            }
+
+            // Delete user here
+
+            return NoContent();
         }
     }
 }
